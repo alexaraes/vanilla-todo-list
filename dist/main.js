59ec2ca3370efb7306fd747adf394152ce9e543c
Alexa Schreffler <alexa.schreffler@gmail.com>
2015-05-25 14:39:50 -0500

    final vanilla to-do list

--- dist/main.js
+++ dist/main.js
@@ -1,16 +1,62 @@
-var list = [];
-var myButton = document.getElementById('clickme');
-var myInput = document.getElementById('input');
-var myList = document.getElementById('todolist');
+// var list = [];
+// var myButton = document.getElementById('clickme');
+// var myInput = document.getElementById('input');
+// var myList = document.getElementById('todolist');
+// myButton.addEventListener('click', onButtonClick);
+// myInput.addEventListener('click', onInput);
+
+// function onInput(); {
+// 	list.push(myInput.value());
+// 	myList.innerHTML = list;
+// 	myInput.innerHTML = '';
+// }
+
+// function onButtonClick(); {
+
+// }
+
+
+
+var myButton = document.getElementById('click-me');
+var messageList = document.getElementById('todo-list');
+var messageInput = document.getElementById('input');
+var fullList = [];
 myButton.addEventListener('click', onButtonClick);
-myInput.addEventListener('click', onInput);
+messageList.addEventListener('click', addStrike);
 
-function onInput(); {
-	list.push(myInput);
-	myList.innerHTML = list.value;
-	myInput.innerHTML = '';
+// var state = false;
+// myButton.innerHTML = 'Add a To-do Item!';
+
+// function addItem(e) {
+// 	fullList.push(messageInput.value);
+// 	console.log(fullList);
+// 	messageInput.innerHTML = '';
+// }
+
+// function addItem(e) {
+// 	fullList.push(messageInput.value);
+// 	// if (messageInput > 0) {
+// 	// 	messageInput.innerHTML = '';
+// 	// }
+// 	console.log(fullList);
+// }
+
+function onButtonClick(eventObj) {
+	
+		
+	messageList.innerHTML += '<div>' + messageInput.value + '</div>';
+	fullList.push(messageInput.value);
+	messageInput.value = '';
+	console.log(fullList);
+	// messageList.innerHTML = messageList.innerHTML + '<div>' + messageInput.value + '</div>';
+
+	// console.log(messageInput.value);
 }
 
-function onButtonClick(); {
-
+function addStrike(){
+	messageList
 }
+
+
+// var parent = document.getElementById('parent');
+// parent.addEventListener('click', onButtonClick);
